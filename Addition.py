95a10406de61e68d6394bad5054301d369d71d9f
Namratarr <93432384+Namratarr@users.noreply.github.com>
2022-01-26 18:29:31 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,18 @@
+def Addition(value1,value2):
+	ans = value1+value2
+	return ans
+
+
+def main():
+	no1=int(input("Enter First Number: "))
+	no2=int(input("Enter second Number: "))
+
+	ret = Addition(no1,no2)
+
+print("start of program...")
+
+if __name__ == '__main__':
+	main();#function call
+
+
+print("End of program...")
