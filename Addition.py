25456f440bab010edd24dc6c39ff62c6623b0963
Namratarr <93432384+Namratarr@users.noreply.github.com>
2022-01-26 18:31:51 +0530

    Update Addition.py

--- Addition.py
+++ Addition.py
@@ -1,8 +1,11 @@
+#This program add two integers
+
+#Addition function which takes two position arguments
 def Addition(value1,value2):
 	ans = value1+value2
 	return ans
 
-
+#main function to accept two parametars
 def main():
 	no1=int(input("Enter First Number: "))
 	no2=int(input("Enter second Number: "))
